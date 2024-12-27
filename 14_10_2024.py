aba7bcd91beb9f33c3f71b629a879ef6e093c543
Daniil Zimin <danya1zimin@gmail.com>
2024-12-27 02:20:11 +0200

    Added a description of the product functionality, and code for implementation

--- 14_10_2024.py
+++ 14_10_2024.py
@@ -1,1 +1,9 @@
 #Calculator
+#The user can add two integers that are between -99 and 99
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+sum_of_nums = num1 + num2
+
+print(sum_of_nums)
