a2c8397ec8b4acaf3f677c2e7449fbe542a5c107
Daniil Zimin <danya1zimin@gmail.com>
2024-12-27 02:33:54 +0200

    The developers changed their minds about the calculator and decided to make a pyramid of stars.

--- 14_10_2024.py
+++ 14_10_2024.py
@@ -1,9 +1,8 @@
-#Calculator
-#The user can add two integers that are between -99 and 99
+#Pyramid
 
-num1 = int(input("Enter the first integer: "))
-num2 = int(input("Enter the second integer: "))
+a = ''
 
-sum_of_nums = num1 + num2
-
-print(sum_of_nums)
+for i in range (0, 5):
+	i += 1
+	a += '*'
+	print(a)
